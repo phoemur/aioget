524e1618abaf330106ed7f1b413900fbf0be3d31
phoemur <phoemur@gmail.com>
2017-07-22 22:14:08 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,10 @@ setup(name='aioget',
           # -*- Extra requirements: -*-
       ],
 
-      long_description='''
+      long_description='''This is a simple script that implements concurrent downloads with Python.
+It makes use of Python >= 3.5 new module's asyncio coroutines with async / await statements.
+Uses aiohttp for assyncronous downloads, aiofiles for nonblocking filesystem operations.
+Has a nice curses based progress_bar with multiple lines
       ''',
     
       entry_points={
