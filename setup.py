5ed3e773f02656ded2ed2fb3bfd2839990d223b2
phoemur <phoemur@gmail.com>
2017-07-22 22:21:01 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(name='aioget',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Networking',
         'Topic :: Utilities',
